9ab7deea7d4fe2d57a7ae7b356d79629af2cc5d9
Bruno Becoski <brunobecoski@gmail.com>
2021-05-25 23:38:30 -0300

    Adição de testes no SubscribeButton

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -3,9 +3,13 @@ import { mocked } from 'ts-jest/utils';
 import { signIn, useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import { SubscribeButton } from '.';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 jest.mock('next-auth/client');
 jest.mock('next/router');
+jest.mock('../../services/api');
+jest.mock('../../services/stripe-js');
 
 describe('SubscribeButton component', () => {
   it('renders correctly', () => {
@@ -61,5 +65,72 @@ describe('SubscribeButton component', () => {
     fireEvent.click(subscribeButton)
 
     expect(pushMock).toHaveBeenCalledWith('/posts');
+  });
+  
+  it('redirect user to stripe', async () => {
+    const useSessionMocked = mocked(useSession);
+    const apiPostMocked = mocked(api.post);
+    const getStripeJsMocked = mocked(getStripeJs);
+
+    const redirectToCheckoutMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'John Doe', 
+          email: 'john.doe@example.com'
+        },
+      },
+      false
+    ])
+
+    apiPostMocked.mockResolvedValueOnce({ 
+      data: { 
+        sessionId: 'fake-session-id'
+      }
+    });
+
+    getStripeJsMocked.mockResolvedValueOnce({
+      redirectToCheckout: redirectToCheckoutMock,
+    } as any);
+
+    render(<SubscribeButton />)
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton)
+
+    setTimeout(() => {
+      expect(redirectToCheckoutMock).toHaveBeenCalled();
+    }, 1000);
+  });
+
+
+  it('error when redirect user to stripe', () => {
+    const useSessionMocked = mocked(useSession);
+
+    const windowAlertMock = jest.fn();
+
+    window.alert = windowAlertMock;
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'John Doe', 
+          email: 'john.doe@example.com'
+        },
+      },
+      false
+    ])
+
+    render(<SubscribeButton />)
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton)
+
+    setTimeout(() => {
+      expect(windowAlertMock).toHaveBeenCalled()
+    }, 1000);
   });
 });
